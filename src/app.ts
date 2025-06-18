1f21b713dc71bfa331654e2043305810ae8a0fd5
WaiYanHtun <waiyanhtun282@gmail.com>
2025-06-18 19:52:31 +0400

    266customerrequire

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,28 @@
-import  express, { urlencoded }  from "express";
+import express, { urlencoded } from "express";
+import { Response, Request } from "express";
 import helmet from "helmet";
 import compression from "compression";
 import cors from "cors";
 import morgan from "morgan";
-import { limiter } from "./middlewares/rateLimit";
-export const app =express();
-app.use(morgan("dev"))
-   .use(urlencoded({ extended: true }))
-   .use(express.json())
-   .use(cors())
-   .use(helmet())
-   .use(compression())
-   .use(limiter)
+import { limiter } from "./middlewares/rateLimiter";
 
+import { check } from "./middlewares/check";
+export const app = express();
+app
+  .use(morgan("dev"))
+  .use(urlencoded({ extended: true }))
+  .use(express.json())
+  .use(cors())
+  .use(helmet())
+  .use(compression())
+  .use(limiter);
 
-   app.get("/health", (req, res) => {
-     res.status(200).json({mesage: "Server is running fine!"});
-   })
+interface CustomRequest extends Request {
+  userId?: number;
+}
+app.get("/health", check, (req: CustomRequest, res: Response) => {
+  res.status(200).json({
+    mesage: "Server is running fine!",
+    user: req.userId,
+  });
+});
---
+++ src/middlewares/check.ts
@@ -0,0 +1,9 @@
+import {Request,Response,NextFunction} from 'express';
+interface CustomRequest extends Request {
+    userId?: number;
+}
+export const check  = (req:CustomRequest,res:Response,next:NextFunction) => {
+    req.userId=12345;
+    next();
+
+}
