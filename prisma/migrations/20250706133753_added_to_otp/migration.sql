5e536d2b1950308ef2ba26ec43dfe9654060958c
WaiYanHtun <waiyanhtun282@gmail.com>
2025-07-06 17:42:56 +0400

    301 Renamemigration

--- prisma/migrations/20250706133753_added_to_otp/migration.sql
+++ prisma/migrations/20250706133753_added_to_otp/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE "Otp" (
     "phone" VARCHAR(15) NOT NULL,
     "otp" VARCHAR(6) NOT NULL,
     "rememberToken" TEXT NOT NULL,
-    "verifyToken" TEXT,
+    "verifyTokens" TEXT,
     "count" SMALLINT NOT NULL DEFAULT 0,
     "error" SMALLINT NOT NULL DEFAULT 0,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
