c00508173853a267d950c49af429722eb80cf02a
WaiYanHtun <waiyanhtun282@gmail.com>
2025-08-12 20:17:24 +0400

    migrate405 reset

--- prisma/generated/prisma/index.js
+++ prisma/generated/prisma/index.js
@@ -268,7 +268,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
